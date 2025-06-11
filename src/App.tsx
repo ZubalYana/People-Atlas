c047399429fd8f11c77ecce888fb1e05eabb4fed
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-06-11 06:06:50 +0300

    apply fonts and some basic styling, create auth required files and foundamental components and routes structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import './App.css'
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Auth from './components/AuthComponents/Auth'
 function App() {
 
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element="Hello" />
+          <Route path='/auth' element={<Auth />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/components/Sidemenu.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { X } from 'lucide-react'
+export default function Sidemenu() {
+    return (
+        <div className='w-[500px] h-full bg-white rounded-md'>
+            <X />
+        </div>
+    )
+}
